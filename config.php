c4c52a62e7cfd8918e7c9dee706b12d692baf5e6
libjkjdxtj <2601454425@qq.com>
2018-11-19 18:57:41 +0800

    数据库连接问题

--- config.php
+++ config.php
@@ -2,5 +2,5 @@
 	header("Content-Type:text/html;charset=utf8");
 	define('HOST','127.0.0.1');//服务器地址
 	define('USERNAME','root');//数据库用户名
-	define('PASSWORD','168168');//数据库密码
+	define('PASSWORD','');//数据库密码
  ?>
